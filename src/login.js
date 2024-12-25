b3554f689ed38b3356bcc57c284c35855603f6ff
Antonio Pablo <pabloisaias2020@gmail.com>
2024-12-25 20:49:23 -0300

    update login.js
    Tela praticamente pronta.

--- src/login.js
+++ src/login.js
@@ -1,23 +1,58 @@
-import { View, TextInput, StyleSheet } from "react-native"
+import {
+  View,
+  TextInput,
+  StyleSheet,
+  TouchableOpacity,
+  Text,
+} from "react-native";
 
-const Login = ()=>{
-    return <View style={styles.container}>
-        <TextInput style={styles.input} placeholder="Usuario"></TextInput>
+const Login = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.textoLogin} >TELA LOGIN DO APP </Text>
+      <TextInput style={styles.input} placeholder="Usuário"></TextInput>
+      <TextInput style={styles.input} placeholder="Senha"></TextInput>
+      <TouchableOpacity style={styles.botao}>
+        <Text style={styles.text}>Entrar</Text>
+      </TouchableOpacity>
     </View>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-        backgroundColor: "white",
-    },
-    input: {
-        width: "80%",
-        backgroundColor: "gray",
-        padding: 15,
-        marginBottom: 10,
-        borderRadius: 10,
-    },
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "white",
+  },
+  input: {
+    width: "80%",
+    backgroundColor: "gray",
+    padding: 10,
+    marginBottom: 10,
+    borderRadius: 10,
+    fontSize: 18,
+  },
+  botao: {
+    width: "20%",
+    backgroundColor: "#d3d3d3",
+    padding: 10,
+    fontSize: 18,
+    borderRadius: 10,
+    marginBottom: 10,
+    
+  },
+  text: {
+    color: "white",
+    fontSize: 18,
+    fontFamily: "Arial-bold",
+    textAlign: "center",
+  },
+  textoLogin: {
+    fontSize: 24,
+    color: "black",
+    marginBottom: 20,
+    fontWeight: "bold",
+  },
 });
 export default Login;
